6770063c35cc2f9586f4f275821bb9cf539572d7
YRollHid <34071152+YRollHid@users.noreply.github.com>
2021-10-28 13:20:23 +0200

    Add create AKS cluster

--- azure_deployment/azure-cli-deploy.sh
+++ azure_deployment/azure-cli-deploy.sh
@@ -21,3 +21,23 @@ SUBNET_ID=$(az network vnet subnet show \
     --vnet-name $VNET_NAME \
     --name $SUBNET_NAME \
     --query id -o tsv)
+
+# Set the AKS cluster name
+AKS_CLUSTER_NAME=aksworkshop-$RANDOM
+echo $AKS_CLUSTER_NAME
+
+# Create the AKS cluster
+az aks create \
+--resource-group $RESOURCE_GROUP \
+--name $AKS_CLUSTER_NAME \
+--vm-set-type VirtualMachineScaleSets \
+--node-count 2 \
+--load-balancer-sku standard \
+--location $REGION_NAME \
+--kubernetes-version $VERSION \
+--network-plugin azure \
+--vnet-subnet-id $SUBNET_ID \
+--service-cidr 10.2.0.0/24 \
+--dns-service-ip 10.2.0.10 \
+--docker-bridge-address 172.17.0.1/16 \
+--generate-ssh-keys
