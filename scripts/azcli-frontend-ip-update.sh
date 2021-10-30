4059ff35a495f01f9e7389c0f3def726386e977b
YRollHid <yans.roll@gmail.com>
2021-10-30 17:27:30 +0200

    Add script to update frontendIP variable

---
+++ scripts/azcli-frontend-ip-update.sh
@@ -0,0 +1,15 @@
+az aks get-credentials -g ${AKSRG} -n ${AKSCLUSTERNAME} --admin
+
+ip=""
+while [ -z $ip ]; do
+  echo "Waiting for external IP"
+  ip=$(kubectl get svc nginx-ingress-ingress-nginx-controller --namespace ingress --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  [ -z "$ip" ] && sleep 10
+done
+echo 'Found external IP: '$ip
+
+echo ${AZURE_DEVOPS_CLI_PAT} | az devops login
+
+az devops configure --defaults organization=https://dev.azure.com/${AZORG} project=${AZPROJECT} --use-git-aliases true
+
+az pipelines variable update --name 'frontendIP' --value $ip
